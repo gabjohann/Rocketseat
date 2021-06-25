dc211d9e804ad0b5a9ffe0cfb3b8da426685a4a6
gabjohann <gabif.johann@gmail.com>
2021-06-24 21:30:35 -0300

    aula 05

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* mudança do header ao scrollar */
-function changeHeaderWhencroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -31,16 +31,22 @@ function changeHeaderWhencroll() {
 
 /* TESTIMONIALS CAROUSEL / SLIDER SWIPER */
 const swiper = new Swiper('.swiper-container', {
-  slidePerView: 1,
+  slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoint: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* SCROLLREVEAL: MOSTRAR ELEMENTOS AO DAR SCROLL NA PÁGINA */
-const scrollReveal = scrollReveal({
+const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700,
@@ -59,18 +65,44 @@ scrollReveal.reveal(
 )
 
 /* BOTÃO DE RETORNAR AO TOPO DA PÁGINA */
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
-    if (window.scrollY >= 560) {
-      backToTopButton.classList.add('show')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* MENU ATIVO CONFORME SEÇÃO VISÍVEL NA PÁGINA */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
     } else {
-      backToTopButton.classList.remove('show')
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
     }
+  }
 }
 
 /* QUANDO SCROLL */
 window.addEventListener('scroll', function () {
-  changeHeaderWhencroll()
-  backToTop ()
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
